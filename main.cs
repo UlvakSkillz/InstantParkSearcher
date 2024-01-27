ae04adb93f7e3e07e0199974f9c0597a774ff62a
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-26 17:05:34 -0800

    Initial commit

---
+++ main.cs
@@ -0,0 +1,37 @@
+﻿using MelonLoader;
+using RUMBLE.Environment;
+using UnityEngine;
+
+namespace InstantParkSearcher
+{
+    public class main : MelonMod
+    {
+        ParkBoardGymVariant parkBoardGymVariant;
+        private string currentScene = "";
+        private bool sceneChanged = false;
+
+        public override void OnFixedUpdate()
+        {
+            if (sceneChanged)
+            {
+                try
+                {
+                    if (currentScene == "Gym")
+                    {
+                        parkBoardGymVariant = GameObject.Find("--------------LOGIC--------------/Heinhouser products/Parkboard").GetComponent<ParkBoardGymVariant>();
+                        parkBoardGymVariant.OnHostJoinSliderChanged(1);
+                        parkBoardGymVariant.OnFindRandomParkPressed();
+                        sceneChanged = false;
+                    }
+                }
+                catch { }
+            }
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+        }
+    }
+}
